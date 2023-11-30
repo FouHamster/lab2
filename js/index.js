2335b72aa66f48ee0a423f7eb2ead6df2bab7dc0
FouHamster <alina63111@gmail.com>
2023-11-30 14:06:43 +0700

    js

---
+++ js/index.js
@@ -0,0 +1,29 @@
+function UserPhone(fname, name, oname, tel, postalCode, country, city, street, house, apartment) {
+    this.fname = fname
+    this.name = name
+    this.oname = oname
+    this.tel = tel
+    this.address = {
+        postalCode: postalCode,
+        country: country,
+        city: city,
+        street: street,
+        house: house,
+        apartment: apartment,
+    }
+}
+
+let user = []
+
+function createNewUser {
+    let fname = document.getElementById('').value 
+    let name = document.getElementById('').value
+    let oname = document.getElementById('').value
+    let tel = document.getElementById('').value
+    let postalCode = document.getElementById('').value
+    let country = document.getElementById('').value
+    let city = document.getElementById('').value
+    let street = document.getElementById('').value
+    let house = document.getElementById('').value
+    let apartment = document.getElementById('').value
+}
