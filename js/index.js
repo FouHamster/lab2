40852c7be9211627f01d4b7ec609acd9e7ce195e
FouHamster <alina63111@gmail.com>
2023-12-11 13:28:18 +0700

    prodol

--- js/index.js
+++ js/index.js
@@ -16,30 +16,24 @@ function UserPhone(surname, name, middleName, tel, postalCode, country, city, st
 let user = []
 
 function createNewUser () {
-    // let surname = document.getElementById('surname').value 
-    // let name = document.getElementById('name').value
-    // let middleName = document.getElementById('middleName').value
-    // let tel = document.getElementById('tel').value
-    // let postalCode = document.getElementById('postalCode').value
-    // let country = document.getElementById('country').value
-    // let city = document.getElementById('city').value
-    // let street = document.getElementById('street').value
-    // let house = document.getElementById('house').value
-    // let apartment = document.getElementById('apartment').value
-    let surname = prompt('Введите фамилию!')
-    let name = prompt('Введите имя!')
-    let middleName = prompt('Введите отчество!')
-    let tel = prompt('Введите номер телефона!')
-    let postalCode = prompt('Введите индекс!')
-    let country = prompt('Введите страну!')
-    let city = prompt('Введите город!')
-    let street = prompt('Введите улицу!')
-    let house = prompt('Введите дом!')
-    let apartment = prompt('Введите квартиру!')
+    let question = confirm('Хотите продолжить?')
+    while (question !== false) {
+        let surname = prompt('Введите фамилию!')
+        let name = prompt('Введите имя!')
+        let middleName = prompt('Введите отчество!')
+        let tel = prompt('Введите номер телефона!')
+        let postalCode = prompt('Введите индекс!')
+        let country = prompt('Введите страну!')
+        let city = prompt('Введите город!')
+        let street = prompt('Введите улицу!')
+        let house = prompt('Введите дом!')
+        let apartment = prompt('Введите квартиру!')
 
-    let users = new UserPhone(surname, name, middleName, tel, postalCode, country, city, street, house, apartment)
-    user.push(users)
-    console.log(users)
+        let users = new UserPhone(surname, name, middleName, tel, postalCode, country, city, street, house, apartment)
+        user.push(users)
+        console.log(users)
+        question = confirm('Хотите продолжить?')
+    }
 }
 
 
